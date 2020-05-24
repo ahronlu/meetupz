b4a416d8c36f3ffefc685763f3353f5aac8aa407
koftov <ahronlu@gmail.com>
2020-05-24 18:25:53 +0300

    1

---
+++ client/precache-manifest.f44dda94344651b3821d023c25854391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103988511eb3751ebec842d2098004a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ebf63f4f88298dafac",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c7a318eaf75431caddb8",
+    "url": "/static/js/2.7f19eb1f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.7f19eb1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ebf63f4f88298dafac",
+    "url": "/static/js/main.a86860b7.chunk.js"
+  },
+  {
+    "revision": "33fa7ce9b8ea4027ee16",
+    "url": "/static/js/runtime-main.0f59eb23.js"
+  }
+]);
